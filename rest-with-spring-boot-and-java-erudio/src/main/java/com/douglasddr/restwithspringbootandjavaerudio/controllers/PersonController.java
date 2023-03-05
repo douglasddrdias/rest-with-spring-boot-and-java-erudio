47d0ffd371b3607b5613676b04b8b280d031407c
douglasdias <douglasdias@pop-os.localdomain>
2023-03-04 22:13:25 -0300

    Finalização dos 4 verbos do rest ainda em mock.

--- rest-with-spring-boot-and-java-erudio/src/main/java/com/douglasddr/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/douglasddr/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +27,23 @@ public class PersonController {
 	public List<Person> findAll() {
 		return service.findAll();
 	}
+	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id" ) String id) throws Exception {
 		return service.findById(id);
 	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws Exception {
+		return service.create(person);
+	}
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws Exception {
+		return service.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable(value = "id" ) String id) throws Exception {
+		service.delete(id);
+	}	
 }
