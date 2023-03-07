82176bf350097bd3eedffc16cc6a8e0a1202830b
douglasdias <douglasdias@pop-os.localdomain>
2023-03-07 15:27:23 -0300

    Fianalização da implementação do content type por header.

--- rest-with-spring-boot-and-java-erudio/src/main/java/com/douglasddr/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/douglasddr/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -3,7 +3,6 @@ package com.douglasddr.restwithspringbootandjavaerudio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.douglasddr.restwithspringbootandjavaerudio.data.vo.v1.PersonVO;
 import com.douglasddr.restwithspringbootandjavaerudio.services.PersonServices;
+import com.douglasddr.restwithspringbootandjavaerudio.utils.MediaTypeUtils;
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -24,21 +24,23 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
-	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YAML})
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 	
-	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(value = "/{id}", produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YAML})
 	public PersonVO findById(@PathVariable(value = "id" ) Long id) throws Exception {
 		return service.findById(id);
 	}
 	
-	@PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PostMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YAML}, 
+				 consumes = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YAML})
 	public PersonVO createV2(@RequestBody PersonVO PersonVO) throws Exception {
 		return service.create(PersonVO);
 	}
-	@PutMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PutMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YAML}, 
+			consumes = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YAML})
 	public PersonVO update(@RequestBody PersonVO PersonVO) throws Exception {
 		return service.update(PersonVO);
 	}
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/douglasddr/restwithspringbootandjavaerudio/utils/MediaTypeUtils.java
@@ -0,0 +1,8 @@
+package com.douglasddr.restwithspringbootandjavaerudio.utils;
+
+public class MediaTypeUtils {
+
+	public static final String APPLICATION_YAML = "application/x-yaml";
+	public static final String APPLICATION_JSON = "application/json";
+	public static final String APPLICATION_XML = "application/xml";
+}
